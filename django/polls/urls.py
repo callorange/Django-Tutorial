0a8862a8cae969da99dcfa083c0efbec5234206d
jeongyeob <sjy20000@gmail.com>
2018-02-02 14:34:21 +0900

    polls 뷰 추가

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
